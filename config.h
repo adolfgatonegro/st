ed8bfccbed19a7a4a50cc73102f25525a97fe0f3
adolfgatonegro <github@gatoneg.ro>
2023-03-06 14:52:40 -0600

    rm ShiftMask to scroll up/down with mouse

--- config.h
+++ config.h
@@ -241,9 +241,9 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release  screen */
+	{ XK_NO_MOD,            Button4, kscrollup,      {.i = 1},      0, S_PRI},
+	{ XK_NO_MOD,            Button5, kscrolldown,    {.i = 1},      0, S_PRI},
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
-	{ ShiftMask,            Button4, kscrollup,      {.i = 1},      0, S_PRI},
-	{ ShiftMask,            Button5, kscrolldown,    {.i = 1},      0, S_PRI},
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
